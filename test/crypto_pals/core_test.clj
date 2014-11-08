84a7786df944c25944ea874b4344111b634f1c7e
Kevin Watson <kevin@watsonkp.com>
2014-11-08 09:16:58 -0500

    Added solution for Set 1 Challenge 8 "Detect AES in ECB mode". All of the messages contained 15 distinct 16 byte blocks except for one, which contained only 12. The single outlier appears to be the ECB cipher, despite the lack of Vanilla Ice.

--- test/crypto_pals/core_test.clj
+++ test/crypto_pals/core_test.clj
@@ -1,7 +1,7 @@
 (ns crypto-pals.core-test
   (:require [clojure.test :refer :all]
             [crypto-pals.core :refer :all]
-            [clojure.data.codec.base64 :as b64 :refer :all] ))
+            [clojure.data.codec.base64 :as b64 :refer :all]))
 
 (def set-1-challenge-1-input "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
 (def set-1-challenge-1-output "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
@@ -136,3 +136,37 @@
                                                   (.doFinal cipher byte-message)))]
                decrypted-message))
            (slurp set-1-challenge-7-decrypted-message)))))
+
+(def set-1-challenge-8-messages "test/crypto_pals/set-1-challenge-8.txt")
+(def set-1-challenge-8-ecb-message (str "d880619740a8a19b7840a8a31c810a3d"
+                                        "08649af70dc06f4fd5d2d69c744cd283"
+                                        "e2dd052f6b641dbf9d11b0348542bb57"
+                                        "08649af70dc06f4fd5d2d69c744cd283"
+                                        "9475c9dfdbc1d46597949d9c7e82bf5a"
+                                        "08649af70dc06f4fd5d2d69c744cd283"
+                                        "97a93eab8d6aecd566489154789a6b03"
+                                        "08649af70dc06f4fd5d2d69c744cd283"
+                                        "d403180c98c8f6db1f2a3f9c4040deb0"
+                                        "ab51b29933f2c123c58386b06fba186a"))
+(deftest set-1-challenge-8-simple
+  (testing "Detect AES in ECB mode"
+    (is (= (let
+             [ranking (sort-by first
+                               (with-open [r (clojure.java.io/reader set-1-challenge-8-messages)]
+                                 (doall (for [message (line-seq r)]
+                                          (let
+                                            [message-bytes (b64/decode (.getBytes message))
+                                             message-blocks (partition 16 message-bytes)
+                                             distinct-blocks (count
+                                                              (frequencies message-blocks))]
+                                            (vector distinct-blocks
+                                                    (count message-blocks)
+                                                    message-bytes))))))
+              ecb-message (apply str (map char
+                                          (b64/encode (last (first ranking)))))
+              _ (println (format "%d of %d blocks are distinct in %s"
+                                 (first (first ranking))
+                                 (second (first ranking))
+                                 ecb-message))]
+             ecb-message)
+           set-1-challenge-8-ecb-message))))
